bb80dd63101dedf4b68f4526e84be10c12853a27
denisseai <msdenisseanaya@gmail.com>
2020-05-09 14:07:59 -0700

    Added methods index & show for trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,14 @@
 class TripsController < ApplicationController
 
-	def index
+  def index
+    
 	end
 	
   def show
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
 	end
 	
   def new
