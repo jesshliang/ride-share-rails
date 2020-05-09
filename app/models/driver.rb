51afbe93d060d1525614d34f14f3ffa8b2ecfffe
denisseai <msdenisseanaya@gmail.com>
2020-05-09 13:35:07 -0700

    Added average rating to driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,18 @@
 class Driver < ApplicationRecord
 	has_many :trips
+	
+	def average_rating
+    if self.trips.empty?
+      average = nil
+    else
+      total_rating = 0
+      counter = 0
+      self.trips.each do |trip|
+        total_rating += trip.rating
+        counter += 1
+      end
+      average = (total_rating / counter)
+    end
+    return average
+  end
 end
