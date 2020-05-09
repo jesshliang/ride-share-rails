d6abfcf57c9bb697e26e604b2c82d3ae3b6571bd
denisseai <msdenisseanaya@gmail.com>
2020-05-09 14:20:02 -0700

    Added some more tests to trips

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -3,29 +3,59 @@ require "test_helper"
 describe TripsController do
   describe "show" do
     it "responds with success when showing an existing valid trip" do
-      # Ensure that there is a trip saved
       driver = Driver.create(name: 'Leroy Jenkins', vin: 'SU9PYDRK6214WL15M')
       passenger = Passenger.create(name: "test person", phone_num: "1234567")
       trip = Trip.create(date: DateTime.now, rating: 2, cost: 100, driver_id: driver.id, passenger_id: passenger.id)
 
-      # Act
       get trip_path(trip.id)
 
-      # Assert
       must_respond_with :success
     end
 
     it "responds with 404 with an invalid trip id" do
-      # Act
       get trip_path(-1)
-      
-      # Assert
+
       must_respond_with :not_found
     end
   end
 
   describe "create" do
-    # Your tests go here
+    it "can create a new trip with valid information accurately, and redirect" do
+      driver = Driver.create(name: 'Leroy Jenkins', vin: 'SU9PYDRK6214WL15M')
+      passenger = Passenger.create(name: "test person", phone_num: "1234567")
+
+      data_hash = {
+        trip: {
+          date: DateTime.now,
+          rating: 2,
+          cost: 100,
+          driver_id: driver.id,
+          passenger_id: passenger.id,
+        }
+      }
+
+      expect {
+        post trips_path, params: data_hash
+      }.must_change 'Trip.count', 1
+
+      new_trip = Trip.first
+      expect(new_trip.rating).must_equal data_hash[:trip][:rating]
+      expect(new_trip.cost).must_equal data_hash[:trip][:cost]
+      expect(new_trip.driver_id).must_equal data_hash[:trip][:driver_id]
+      expect(new_trip.passenger_id).must_equal data_hash[:trip][:passenger_id]
+
+      must_redirect_to passenger_path(new_trip.passenger_id)
+    end
+
+    it "does not create a trip if the form data violates Trip validations, and responds with a redirect" do
+      data_hash = {}
+
+      expect {
+        post trips_path, params: data_hash
+      }.wont_change 'Trip.count'
+
+      must_redirect_to root_path
+    end
   end
 
   describe "edit" do
