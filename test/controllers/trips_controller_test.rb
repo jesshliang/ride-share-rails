15fee513941c383f2d90bb0b571ec2bce07bd1b7
denisseai <msdenisseanaya@gmail.com>
2020-05-09 13:58:21 -0700

    Added some tests for trip

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -2,7 +2,26 @@ require "test_helper"
 
 describe TripsController do
   describe "show" do
-    # Your tests go here
+    it "responds with success when showing an existing valid trip" do
+      # Ensure that there is a trip saved
+      driver = Driver.create(name: 'Leroy Jenkins', vin: 'SU9PYDRK6214WL15M')
+      passenger = Passenger.create(name: "test person", phone_num: "1234567")
+      trip = Trip.create(date: DateTime.now, rating: 2, cost: 100, driver_id: driver.id, passenger_id: passenger.id)
+
+      # Act
+      get trip_path(trip.id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "responds with 404 with an invalid trip id" do
+      # Act
+      get trip_path(-1)
+      
+      # Assert
+      must_respond_with :not_found
+    end
   end
 
   describe "create" do
