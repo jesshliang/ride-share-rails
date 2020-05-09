0176f313d4576e7adcf19407168273e48cbd1b87
Jessica Liang <jessicursliang@gmail.com>
2020-05-09 14:38:15 -0700

    update nav links, add driver routes, update driver show

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,24 @@ class Driver < ApplicationRecord
 
 	validates :name, presence: true
 	validates :vin, presence: true
+
+	def average_rating
+		sum_rating = 0
+
+		self.trips.each do |trip|
+			sum_rating += trip.rating
+		end
+
+		return (sum_rating / self.trips.length)
+	end
+
+	def total_earnings
+		total = 0
+
+		self.trips.each do |trip|
+			total += trip.cost
+		end
+
+		return total
+	end
 end
